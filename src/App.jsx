60f8ba062c182529c5f5615907dd3e9896eb1a26
Texterly <88582978+Texterly@users.noreply.github.com>
2022-11-27 18:45:40 +0300

    added new logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import hotBg from './assets/hot.jpg';
+import coldBg from './assets/cold.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="App" style={{ backgroundImage: `url(${coldBg})` }}>
+      <div className="overlay">
+        <div className="container">
+          <div className="section section_inputs">
+            <input type="text" name="city" placeholder="Enter city..." />
+            <button>F</button>
+          </div>
+          <div className="section section_temperature">
+            <div className="icon">
+              <h3>London, GB</h3>
+              <img src="https://www.modlabs.net/uploads/gallery/blogs/the-cloud.jpg" alt="weatherIcon" />
+              <h3>Cloudy</h3>
+            </div>
+            <div className="temperature">
+              <h1>34 C</h1>
+            </div>
+          </div>
+          {/* bottom description */}
+        </div>
+      </div>
     </div>
   );
 }
